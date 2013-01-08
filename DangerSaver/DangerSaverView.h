310f6f8199c74087cedc182a5a3a5f47ae61eab2
Chris Sinchok <csinchok@theonion.com>
2013-01-07 18:19:00 -0600

    Initial Commit

---
+++ DangerSaver/DangerSaverView.h
@@ -0,0 +1,13 @@
+//
+//  DangerSaverView.h
+//  DangerSaver
+//
+//  Created by Chris Sinchok on 1/7/13.
+//  Copyright (c) 2013 Chris Sinchok. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface DangerSaverView : ScreenSaverView
+
+@end
---
+++ DangerSaver/DangerSaverView.m
@@ -0,0 +1,52 @@
+//
+//  DangerSaverView.m
+//  DangerSaver
+//
+//  Created by Chris Sinchok on 1/7/13.
+//  Copyright (c) 2013 Chris Sinchok. All rights reserved.
+//
+
+#import "DangerSaverView.h"
+
+@implementation DangerSaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
